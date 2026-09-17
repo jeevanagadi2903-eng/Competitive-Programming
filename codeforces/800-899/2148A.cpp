8d116ec913299183b90a299ab9ccfbd66c394781
Jeevan J Agadi <jeevanagadi2903@gmail.com>
2026-09-17 23:22:23 +0530

    Solved: 2148A - Sublime Sequence (codeforces)

---
+++ codeforces/800-899/2148A.cpp
@@ -0,0 +1,10 @@
+// Problem: 2148A - Sublime Sequence
+// Platform: codeforces
+// Contest: Contest-2148
+// Rating/Difficulty: 800
+// Language: C++23 (GCC 14-64, msys2)
+// Verdict: Accepted
+// URL: https://codeforces.com/contest/2148/submission/2148
+// Solved on: 2026-09-17T17:52:20.914Z
+
+// source not captured automatically - open the submission page and copy it manually
