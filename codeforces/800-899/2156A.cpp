0a14df3417a7fc2ca8749d9fcd82fc14a8be3669
Jeevan J Agadi <jeevanagadi2903@gmail.com>
2026-09-20 23:01:20 +0530

    Solved: A - Pizza Time (codeforces)

---
+++ codeforces/800-899/2156A.cpp
@@ -0,0 +1,10 @@
+// Problem: A - Pizza Time
+// Platform: codeforces
+// Contest: Contest-2156
+// Rating/Difficulty: 800
+// Language: C++23 (GCC 14-64, msys2)
+// Verdict: Accepted
+// URL: https://codeforces.com/contest/2156/submission/391353105
+// Solved on: 2026-09-20T17:31:21.018Z
+
+// source not captured automatically - open the submission page and copy it manually
